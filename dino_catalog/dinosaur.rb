64a9ac8fe11e3d914e6ee1b16c769fa072c06821
Raj Singh <raj@foreversingh.com>
2015-07-02 13:19:19 -0500

    making the model less dumb

--- dino_catalog/dinosaur.rb
+++ dino_catalog/dinosaur.rb
@@ -1,6 +1,6 @@
 class Dinosaur
   attr_reader :name, :period, :continent, :diet, :weight_in_lbs, :walking, :description
-  
+
   def initialize(args={})
     @name = args[:name]
     @period = args[:period]
@@ -9,11 +9,25 @@ class Dinosaur
     @weight_in_lbs = args[:weight_in_lbs]
     @walking = args[:walking]
     @description = args[:description]
+    
+    validate_weight!(@weight_in_lbs)
+    validate_description!(@description)
   end
   
+  def is_carnivore?
+    if self.diet != "Carnivore" || self.diet != nil
+    end
+  end
+  
+  def is_biped?
+    if self.walking == "Biped"
+    end
+  end
+  
+
   def to_json
     dinosaur = {
-      name: @name, 
+      name: @name,
       period: @period,
       continent: @continent,
       diet: @diet,
@@ -23,5 +37,20 @@ class Dinosaur
     }
     dinosaur.to_json
   end
+
+  private
+  
+  def validate_weight!(weight)
+    if weight != nil && !(weight.is_a? Integer)
+      raise "Weight must be an integer!"
+    end
+  end
+  
+  def validate_description!(desc)
+    if desc != nil && desc == ""
+      raise "The description cannot be an empty string!"
+    end
+  end
+  
 
 end
